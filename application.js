a5bd26da7cdc9c824808a27950feda74d12dab0b
sbarnham <barnham.s@etoncollege.org.uk>
2022-02-19 17:13:54 +0000

    words get request added

--- application.js
+++ application.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const words = require('./words.json')
 
 app.use(express.static('client'));
 app.use(express.json());
 
 module.exports = app
+
+app.get('/words', function(req,resp) {
+    resp.json(words)
+})
