941b530cdccccdc9e4cc4757ddcfea25ea5b89d5
NotTodaySatan0 <dlxv82@durham.ac.uk>
2022-02-19 17:58:21 +0000

    Added getWord function

--- application.js
+++ application.js
@@ -12,14 +12,32 @@ module.exports = app
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-app.get('/words', function(req,resp) {
+
+
+app.get('/words', function (req, resp) {
     resp.json(words)
 })
 
-app.post('/spoil', function(req, resp) {
+app.post('/spoil', function (req, resp) {
+    const todaysWord = getWord(); 
+
     const numbers = req.body
     for (let number of numbers) {
         twilio.messages
-            .create({body: `${todaysWord}`, from: '+447488884357', to: `${number}`})
+            .create({ body: `${todaysWord}`, from: '+447488884357', to: `${number}` })
     }
 })
+
+function getWord() {
+    var startDate = Date('19/02/2022');
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    var timeDifference = date2.getTime() - date1.getTime();   
+    var daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+    return words[daysDifference];
+};
