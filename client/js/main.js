4ab7c8b64faca4a19f94ed103444986989264695
sbarnham <barnham.s@etoncollege.org.uk>
2022-02-19 21:58:54 +0000

    fixing kade being a div

--- client/js/main.js
+++ client/js/main.js
@@ -59,13 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(number)
     numberlist.push(number)
     var html = `
-<<<<<<< HEAD
     ${number}
-=======
-    <div class="row">
-      ${numberlist[i]}
-    </div>
->>>>>>> 4f8bb1cbd1450c102d782a3981080e32fa7cee89
     `
     numberposition.innerHTML = numberposition.innerHTML + "<br>" + html + "</br>"
   })
